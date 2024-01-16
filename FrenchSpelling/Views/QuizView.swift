5137bd74e958f6ce0dbeab357ca71e8d8cd20787
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2024-01-15 20:09:06 -0500

    Setup the interface for asking a question

--- FrenchSpelling/Views/QuizView.swift
+++ FrenchSpelling/Views/QuizView.swift
@@ -10,8 +10,20 @@ import SwiftUI
 struct QuizView: View {
     var body: some View {
         HStack {
-            Text ("Quiz Interface")
-            
+            //Quiz Interface
+            VStack {
+                HStack {
+                    Spacer()
+                    Text("Banana")
+                        .font(.custom( "Helvetica", size: 96.0))
+                }
+                
+                
+                Divider()
+                
+                
+            }
+            // List of past questions
             Text ("History")
         }
         .frame(width: 800, height: 400)
