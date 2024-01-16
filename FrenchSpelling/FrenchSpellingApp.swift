a45f9597622117acfa0b43f204a7f1ff696dd421
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2024-01-15 19:09:05 -0500

    Initial Commit

---
+++ FrenchSpelling/FrenchSpellingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrenchSpellingApp.swift
+//  FrenchSpelling
+//
+//  Created by Lewis Brocklehurst on 2024-01-15.
+//
+
+import SwiftUI
+
+@main
+struct FrenchSpellingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
