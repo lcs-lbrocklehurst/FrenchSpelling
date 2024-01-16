99266cac0d1ee1feeebce416ed0dc89016b6878e
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2024-01-15 22:12:05 -0500

    Checking the answer given against the correct answer and setting the appropriate result (correct or incorrect)

---
+++ FrenchSpelling/Model/AnswerState.swift
@@ -0,0 +1,14 @@
+//
+//  AnswerState.swift
+//  FrenchSpelling
+//
+//  Created by Lewis Brocklehurst on 2024-01-15.
+//
+
+import Foundation
+
+enum AnswerState: String {
+    case NoInputGiven = ""
+    case incorrect = "❌"
+    case correct = "✅"
+}
--- FrenchSpelling/Views/QuizView.swift
+++ FrenchSpelling/Views/QuizView.swift
@@ -8,10 +8,12 @@
 import SwiftUI
 
 struct QuizView: View {
+    //MARK: Stored properties
     @State var currentWord = vocabularyList.randomElement()!
-    
     @State var answerGiven = ""
+    @State var result: AnswerState = .NoInputGiven
     
+    //MARK: Computed properties
     var body: some View {
         HStack {
             //Quiz Interface
@@ -28,11 +30,33 @@ struct QuizView: View {
                 TextField("Answer", text: $answerGiven)
                     .font(.custom( "Helvetica", size: 96.0))
                     .multilineTextAlignment(.trailing)
+                
+                HStack {
+                    Spacer()
+                    
+                    Button(action: {
+                        //Check answer
+                    }, label: {
+                        Text("Submit")
+                })
+                }
             }
             // List of past questions
             Text ("History")
         }
         .frame(width: 800, height: 400)
+    }
+    
+    //MARK: Functions
+    func checkAnswer() {
+        let correctAnswer = currentWord.french
+        
+        if answerGiven == correctAnswer {
+            // Tell the user they got it right
+            result = .correct
+        } else {
+            result = .incorrect
+        }
     }
 }
 
