01c9c63870134b0fbdc373fc5795753d5e85f2d6
Lewis Brocklehurst <lbrocklehurst@lcs.on.ca>
2024-01-15 20:00:46 -0500

    Organized the project and started to make main interface.

---
+++ FrenchSpelling/Views/QuizView.swift
@@ -0,0 +1,23 @@
+//
+//  QuizView.swift
+//  FrenchSpelling
+//
+//  Created by Lewis Brocklehurst on 2024-01-15.
+//
+
+import SwiftUI
+
+struct QuizView: View {
+    var body: some View {
+        HStack {
+            Text ("Quiz Interface")
+            
+            Text ("History")
+        }
+        .frame(width: 800, height: 400)
+    }
+}
+
+#Preview {
+    QuizView()
+}
